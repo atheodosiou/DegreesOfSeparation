ea713708030b64d18fda6030a81bf66258fd6631
Anastasios Theodosiou <anastasios.theodosiou@gmail.com>
2018-11-14 22:24:39 +0200

    Create main class DegreesOfSeparation.scala

---
+++ src/main/scala/DegreesOfSeparation.scala
@@ -0,0 +1,7 @@
+object DegreesOfSeparation {
+  def main(args: Array[String]): Unit = {
+    println("-------------------------------------------")
+    println("| Find degree of separation between heros |")
+    println("-------------------------------------------")
+  }
+}
